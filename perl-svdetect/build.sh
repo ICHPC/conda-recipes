a8645039cf7db0ae616417a716ec4bbcfbf7a17b
MJ Harvey <m.j.harvey@acellera.com>
2017-11-29 12:11:45 +0000

    svdetect recipt per SR0520279

---
+++ perl-svdetect/build.sh
@@ -0,0 +1,9 @@
+wget 'https://downloads.sourceforge.net/project/svdetect/SVDetect/0.70/SVDetect_r0.7m.tar.gz'
+tar -zxvf *gz
+cd SVDetect_r0.7m
+mkdir -p $PREFIX/bin
+chmod a+rx bin/* scripts/*
+cp bin/* scripts/* $PREFIX/bin
+sed -i 's/#!\/usr\/bin\/perl/#!\/usr\/bin\/env perl/g' $PREFIX/bin
+
+
