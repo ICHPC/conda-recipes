c6eafd4f368a19bba9792f7282bfa5510697a489
MJ Harvey <m.j.harvey@acellera.com>
2017-12-05 15:50:03 +0000

    danpo

--- danpos/build.sh
+++ danpos/build.sh
@@ -1,6 +1,6 @@
 wget http://lilab.research.bcm.edu/dldcc-web/lilab/kaifuc/danpos/release/danpos-2.2.2.tgz
 tar -zxvf danpos-2.2.2.tgz
 mkdir -p $PREFIX/bin
-chmod danpos-2.2.2/bin/*py
+chmod a+x danpos-2.2.2/*py
 cp danpos-2.2.2/* $PREFIX/bin
 
