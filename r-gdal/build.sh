bc4274b00a1423f5c4e0206e1bc8163157f8a76b
slacalle <s.lacalle@imperial.ac.uk>
2018-05-25 11:24:28 +0100

    rgdal recipe now available in conda-forge r-rgdal

--- r-gdal/build.sh
+++
@@ -1,1 +0,0 @@
-$R CMD INSTALL --build --configure-args="--prefix=$PREFIX --with-gdal-config=$PREFIX/bin/gdal-config --with-proj-include=$PREFIX/include --with-proj-lib=$PREFIX/lib --with-proj-share=$PREFIX/share/proj" .
