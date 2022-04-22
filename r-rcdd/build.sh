c3c32f444a956833fc636c7582dd45c68d308c82
Santiago Lacalle <slacalle@imperial.ac.uk>
2022-04-22 15:02:39 +0100

    Updated recipe to 1.5

--- r-rcdd/build.sh
+++ r-rcdd/build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
-  export DISABLE_AUTOBREW=1
-  mv DESCRIPTION DESCRIPTION.old
-  grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-  $R CMD INSTALL --build .
-else
-  mkdir -p $PREFIX/lib/R/library/rcdd
-  mv * $PREFIX/lib/R/library/rcdd
-fi
+# 'Autobrew' is being used by more and more packages these days
+# to grab static libraries from Homebrew bottles. These bottles
+# are fetched via Homebrew's --force-bottle option which grabs
+# a bottle for the build machine which may not be macOS 10.9.
+# Also, we want to use conda packages (and shared libraries) for
+# these 'system' dependencies. See:
+# https://github.com/jeroen/autobrew/issues/3
+export DISABLE_AUTOBREW=1
+
+# R refuses to build packages that mark themselves as Priority: Recommended
+mv DESCRIPTION DESCRIPTION.old
+grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
+# shellcheck disable=SC2086
+${R} CMD INSTALL --build . ${R_ARGS}
+
+# Add more build steps here, if they are necessary.
+
+# See
+# https://docs.conda.io/projects/conda-build
+# for a list of environment variables that are set during the build process.
